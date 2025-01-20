6f6d583140b8581c344e20e0c6a416a12f49d9c7
Marina Fernandes <marinafernandess103@gmail.com>
2025-01-19 21:21:43 -0300

    final

--- main.R
+++ main.R
@@ -1,44 +1,44 @@
-dadoschapeco <- read.csv("dados-chapeco.csv",header = TRUE, skip = 9, sep = ";",dec = ".", check.names = FALSE )
-dados <- read.csv("dados-xanxere.csv",header = TRUE, skip = 9, sep = ";",dec = ".", check.names = FALSE )
-str(dados)
-View(dados)
-str(dadoschapeco)
-View(dadoschapeco)
-dados[9] <- NULL
-names(dados) <- c("Data", "Prec", "TempMax", "TempMedia", "TempMin", "UmidadeMedia", "VentoRajMax", "VentoVelMedia")
-dadoschapeco[9] <- NULL
-names(dadoschapeco) <- c("Data", "Prec", "TempMax", "TempMedia", "TempMin", "UmidadeMedia", "VentoRajMax", "VentoVelMedia")
+dadosc <- read.csv("dados-chapeco.csv",header = TRUE, skip = 9, sep = ";",dec = ".", check.names = FALSE )
+dadosx <- read.csv("dados-xanxere.csv",header = TRUE, skip = 9, sep = ";",dec = ".", check.names = FALSE )
+str(dadosx)
+View(dadosx)
+str(dadosc)
+View(dadosc)
+dadosx[9] <- NULL
+names(dadosx) <- c("Data", "Prec", "TempMax", "TempMedia", "TempMin", "UmidadeMedia", "VentoRajMax", "VentoVelMedia")
+dadosc[9] <- NULL
+names(dadosc) <- c("Data", "Prec", "TempMax", "TempMedia", "TempMin", "UmidadeMedia", "VentoRajMax", "VentoVelMedia")
 
-dados$Data<-as.Date(dados$Data,"%Y-%m-%d")
-dados[,2] <- as.numeric(dados[,2]) 
-dados[,3] <- as.numeric(dados[,3]) 
-dados[,4] <- as.numeric(dados[,4]) 
-dados[,5] <- as.numeric(dados[,5]) 
-dados[,6] <- as.numeric(dados[,6]) 
-dados[,7] <- as.numeric(dados[,7])
-dados[,8] <- as.numeric(dados[,8]) 
+dadosx$Data<-as.Date(dadosx$Data,"%Y-%m-%d")
+dadosx[,2] <- as.numeric(dadosx[,2]) 
+dadosx[,3] <- as.numeric(dadosx[,3]) 
+dadosx[,4] <- as.numeric(dadosx[,4]) 
+dadosx[,5] <- as.numeric(dadosx[,5]) 
+dadosx[,6] <- as.numeric(dadosx[,6]) 
+dadosx[,7] <- as.numeric(dadosx[,7])
+dadosx[,8] <- as.numeric(dadosx[,8]) 
 
-dadoschapeco$Data<-as.Date(dadoschapeco$Data,"%Y-%m-%d")
-dadoschapeco[,2] <- as.numeric(dadoschapeco[,2]) 
-dadoschapeco[,3] <- as.numeric(dadoschapeco[,3]) 
-dadoschapeco[,4] <- as.numeric(dadoschapeco[,4]) 
-dadoschapeco[,5] <- as.numeric(dadoschapeco[,5]) 
-dadoschapeco[,6] <- as.numeric(dadoschapeco[,6]) 
-dadoschapeco[,7] <- as.numeric(dadoschapeco[,7])
-dadoschapeco[,8] <- as.numeric(dadoschapeco[,8])
+dadosc$Data<-as.Date(dadosc$Data,"%Y-%m-%d")
+dadosc[,2] <- as.numeric(dadosc[,2]) 
+dadosc[,3] <- as.numeric(dadosc[,3]) 
+dadosc[,4] <- as.numeric(dadosc[,4]) 
+dadosc[,5] <- as.numeric(dadosc[,5]) 
+dadosc[,6] <- as.numeric(dadosc[,6]) 
+dadosc[,7] <- as.numeric(dadosc[,7])
+dadosc[,8] <- as.numeric(dadosc[,8])
 
-str(dados)
-View(dados)
-str(dadoschapeco)
-View(dadoschapeco)
+str(dadosx)
+View(dadosx)
+str(dadosc)
+View(dadosc)
 
-dados$Dia <- as.numeric(format(dados$Data, format = "%d"))
-dados$Mes <- as.numeric(format(dados$Data, format = "%m"))
-dados$Ano <- as.numeric(format(dados$Data, format = "%Y"))
+dadosx$Dia <- as.numeric(format(dadosx$Data, format = "%d"))
+dadosx$Mes <- as.numeric(format(dadosx$Data, format = "%m"))
+dadosx$Ano <- as.numeric(format(dadosx$Data, format = "%Y"))
 
-dadoschapeco$Dia <- as.numeric(format(dadoschapeco$Data, format = "%d"))
-dadoschapeco$Mes <- as.numeric(format(dadoschapeco$Data, format = "%m"))
-dadoschapeco$Ano <- as.numeric(format(dadoschapeco$Data, format = "%Y"))
+dadosc$Dia <- as.numeric(format(dadosc$Data, format = "%d"))
+dadosc$Mes <- as.numeric(format(dadosc$Data, format = "%m"))
+dadosc$Ano <- as.numeric(format(dadosc$Data, format = "%Y"))
 
 #install.packages("tidyverse")
 #install.packages("plotly")
@@ -46,45 +46,45 @@ dadoschapeco$Ano <- as.numeric(format(dadoschapeco$Data, format = "%Y"))
 library(tidyverse)
 library(plotly)
 
-temperatura <- select(dados, TempMedia)
-temperaturachap <- select(dadoschapeco, TempMedia)
+temperatura <- select(dadosx, TempMedia)
+temperaturachap <- select(dadosc, TempMedia)
 
 mean(temperatura$TempMedia, na.rm=TRUE)
 mean(temperaturachap$TempMedia, na.rm=TRUE)
 
-mean(dados$TempMax, na.rm=TRUE)
-mean(dadoschapeco$TempMax, na.rm=TRUE)
+mean(dadosx$TempMax, na.rm=TRUE)
+mean(dadosc$TempMax, na.rm=TRUE)
 
-mean(dados$TempMin, na.rm=TRUE)
-mean(dadoschapeco$TempMin, na.rm=TRUE)
+mean(dadosx$TempMin, na.rm=TRUE)
+mean(dadosc$TempMin, na.rm=TRUE)
 
-mean(dados$Prec, na.rm=TRUE)
-mean(dadoschapeco$Prec, na.rm=TRUE)
+mean(dadosx$Prec, na.rm=TRUE)
+mean(dadosc$Prec, na.rm=TRUE)
 
-mean(dados$VentoVelMedia, na.rm=TRUE)
-mean(dadoschapeco$VentoVelMedia, na.rm=TRUE)
+mean(dadosx$VentoVelMedia, na.rm=TRUE)
+mean(dadosc$VentoVelMedia, na.rm=TRUE)
 
-mean(dados$UmidadeMedia, na.rm=TRUE)
-mean(dadoschapeco$UmidadeMedia, na.rm=TRUE)
+mean(dadosx$UmidadeMedia, na.rm=TRUE)
+mean(dadosc$UmidadeMedia, na.rm=TRUE)
 
-mean(dados$VentoRajMax, na.rm=TRUE)
-mean(dadoschapeco$VentoRajMax, na.rm=TRUE)
+mean(dadosx$VentoRajMax, na.rm=TRUE)
+mean(dadosc$VentoRajMax, na.rm=TRUE)
 
 resumo<-summary(temperatura$TempMedia)
 resumo
 range(temperatura$Temp, na.rm = TRUE)
 
-View(dados$Ano)
-anos <- dados%>% group_by(Ano)%>%
+View(dadosx$Ano)
+anos <- dadosx%>% group_by(Ano)%>%
   summarise(TempMedia = mean(TempMedia, na.rm=TRUE))
-anosc <- dadoschapeco%>% group_by(Ano)%>%
+anosc <- dadosc%>% group_by(Ano)%>%
   summarise(TempMedia = mean(TempMedia, na.rm=TRUE))
 View(anosc)
 
 
 graficorajada <- plot_ly() %>%
-  add_trace( x = dados$Data[1858:4018], y = dados$VentoRajMax[1858:4018], type = 'scatter', name = 'Xanxerê', marker = list(color = 'red')) %>%
-  add_trace( x = dadoschapeco$Data, y = dadoschapeco$VentoRajMax, type = 'scatter', name = 'Chapecó', marker = list(color = 'blue')) %>%
+  add_trace( x = dadosx$Data[1858:4018], y = dadosx$VentoRajMax[1858:4018], type = 'scatter', name = 'Xanxerê', marker = list(color = 'red')) %>%
+  add_trace( x = dadosc$Data, y = dadosc$VentoRajMax, type = 'scatter', name = 'Chapecó', marker = list(color = 'blue')) %>%
   layout(title = "Velocidade Máxima Rajada de Vento",xaxis = list(title = "Ano"),yaxis = list(title = "Velocidade Máxima Rajada de Vento (m/s)"), barmode = 'group'
   )
 
@@ -92,19 +92,19 @@ graficorajada
 
 
 graficorajadazoom <- plot_ly() %>%
-  add_trace( x = dados$Data[2404:3565], y = dados$VentoRajMax[2404:3565], type = 'scatter', name = 'Xanxerê', marker = list(color = 'red')) %>%
-  add_trace( x = dadoschapeco$Data[499:1660], y = dadoschapeco$VentoRajMax[499:1660], type = 'scatter', name = 'Chapecó', marker = list(color = 'blue')) %>%
+  add_trace( x = dadosx$Data[2404:3565], y = dadosx$VentoRajMax[2404:3565], type = 'scatter', name = 'Xanxerê', marker = list(color = 'red')) %>%
+  add_trace( x = dadosc$Data[499:1660], y = dadosc$VentoRajMax[499:1660], type = 'scatter', name = 'Chapecó', marker = list(color = 'blue')) %>%
   layout(title = "2020 à 2023 Velocidade Máxima Rajada de Vento",xaxis = list(title = "Ano"),yaxis = list(title = "Velocidade Máxima Rajada de Vento (m/s)"), barmode = 'group'
   )
 
 graficorajadazoom
 
-plot(dadoschapeco$Data,dadoschapeco$TempMedia,ylim =c(-5,32),type="l" )
-points(dadoschapeco$Data,dadoschapeco$TempMax,type="l",col="red" )
-points(dadoschapeco$Data,dadoschapeco$TempMin,type="l",col="blue" )
+plot(dadosc$Data,dadosc$TempMedia,ylim =c(-5,32),type="l" )
+points(dadosc$Data,dadosc$TempMax,type="l",col="red" )
+points(dadosc$Data,dadosc$TempMin,type="l",col="blue" )
 
 
-dados_agrupados <- dados %>%
+dados_agrupados <- dadosx %>%
   group_by(Ano) %>%
   summarise(
     TempMax = mean(TempMax, na.rm = TRUE),
@@ -149,7 +149,7 @@ fig <- fig %>% layout(
 
 fig
 
-dados_agrupadosC <- dadoschapeco %>%
+dados_agrupadosC <- dadosc %>%
   group_by(Ano) %>%
   summarise(
     TempMax = mean(TempMax, na.rm = TRUE),
